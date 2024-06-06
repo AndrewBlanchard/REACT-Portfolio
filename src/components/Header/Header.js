2470fc7a74678fa8ba1bf696beade6b22c60abc2
AndrewBlanchard <blanchard0andrew@gmail.com>
2024-06-06 13:57:08 -0500

    Updated App.js and Header.js

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css';
+import Navigation from '../Navigation/Navigation';
+
+function Header() {
+  return (
+    <header>
+      <h1>Developer's Name</h1>
+      <Navigation />
+    </header>
+  );
+}
+
+export default Header;
